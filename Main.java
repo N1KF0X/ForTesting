f24a902ad729fc0139ddf977855c54b177deafbe
N1KF0X <96484000+N1KF0X@users.noreply.github.com>
2022-05-16 18:38:58 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static double Min2(double a, double b){
+        if (a > b){
+            return b;
+        }
+        else {
+            return a;
+        }
+    }
+
+    public static double Max2(double a, double b){
+        if (a < b){
+            return b;
+        }
+        else {
+            return a;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
